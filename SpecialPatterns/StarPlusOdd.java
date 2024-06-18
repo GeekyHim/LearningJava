2709cd1044a68adbe78a230270ddcbf9c7396c86
Himanshu-1720 <work.himanshu1720@gmail.com>
2024-06-18 19:13:43 +0530

    New File

---
+++ SpecialPatterns/StarPlusOdd.java
@@ -0,0 +1,29 @@
+package SpecialPatterns;
+import java.util.Scanner;
+public class StarPlusOdd {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter No of Rows : ");
+        int n = scan.nextInt();
+        if(n%2!=0){
+            for(int i = 1; i<=n ; i++){
+                if(i==(n/2+1)){
+                    for(int j = 1; j<=n ; j++){
+                        System.out.print("* ");
+                    }
+                }
+                else{
+                    for(int k=1; k<=n/2 ;k++)
+                    {
+                        System.out.print("  ");
+                    }
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+        else {System.err.println("Enter Odd Number...");}
+        
+        scan.close();
+    }
+}

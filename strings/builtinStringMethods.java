91c3e4a1e50095c51c41472563f874ff40d3706e
GeekyHim <work.himanshu1720@gmail.com>
2024-12-03 17:56:43 +0530

    Strings

---
+++ strings/builtinStringMethods.java
@@ -0,0 +1,17 @@
+package strings;
+public class builtinStringMethods {
+    public static void main(String[] args) {
+        String s = "Himanshu Gupta";
+        System.out.println(s.charAt(4));
+        // agar uski length se zyada pe charAt() lgaya toh error ayega
+        int n = s.length();
+        System.out.println(n);
+        /*
+         * For any array arr ->
+         * n = arr.length
+         * for any String s
+         * n = s.length()
+         * so we see that in string () is imp.
+         */
+    }
+}

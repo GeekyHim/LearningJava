685080fb971c902efe004ddba000163df097b747
GeekyHim <work.himanshu1720@gmail.com>
2025-05-16 00:29:06 +0530

    Recurrsion, Permutations

---
+++ Recursion2/Permutations.java
@@ -0,0 +1,28 @@
+
+import java.util.ArrayList;
+
+public class Permutations {
+
+    static ArrayList<String> ansList = new ArrayList<>();
+    public static void printPermutations(String ans, String s){
+        if(s.length()==0) {
+            ansList.add(ans);
+            System.out.println(ans);
+            return;
+        }
+
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            String leftString = s.substring(0, i);
+            String righString = s.substring(i+1);
+            printPermutations(ans+ch, leftString+righString);
+        }
+        
+    }
+    public static void main(String[] args) {
+        String s = "abc";
+        printPermutations("",s);
+
+        System.out.println(ansList);
+    }
+}

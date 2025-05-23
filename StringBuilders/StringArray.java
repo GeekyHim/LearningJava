60cf28ee64a4f4bb7ae3dec56acffec957190418
GeekyHim <work.himanshu1720@gmail.com>
2025-05-23 20:18:05 +0530

    String Array

---
+++ StringBuilders/StringArray.java
@@ -0,0 +1,8 @@
+public class StringArray {
+    public static void main(String[] args) {
+        String[] arr = {"himanshu", "raghav", "coding"};
+        // sort of a 2D array we can say since
+        // we can say a string is a char []
+        // hence char[]s ka array
+    }
+}

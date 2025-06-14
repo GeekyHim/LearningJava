fe830237310a65157ccecef89bac3ff7a2a78d03
GeekyHim <work.himanshu1720@gmail.com>
2025-06-14 18:16:01 +0530

    LL

--- LinkedListOne/ImplementationLLDS.java
+++ LinkedListOne/ImplementationLLDS.java
@@ -1,7 +1,11 @@
 public class ImplementationLLDS {
     public static void main(String[] args) {
         MyLinkedListDS list = new MyLinkedListDS();
-        list.add(10);
-        
+        list.addAtLast(10);
+        list.addAtLast(20);
+        list.addAtLast(30);
+        list.addAtLast(40);
+        list.addAtStart(50);
+        list.print();
     }
 }
--- LinkedListOne/MyLinkedListDS.java
+++ LinkedListOne/MyLinkedListDS.java
@@ -8,6 +8,45 @@ class Node{ // user defined data type
 public class MyLinkedListDS {// user defined data Structure
     Node head; // by default is null
     Node tail;
+    int size; // by default is 0
     // My Member Functions
+    void print(){
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+    
+    void size(){
+        System.out.println("Length of LinkedList is: "+size);
+    }
+
+    void addAtLast(int val){
+        Node temp = new Node(val);
+        if(head==null) {
+            head = temp;
+            tail = temp;
+        }else{
+            tail.next = temp;
+            // temp ka next is alrdy null 
+            //temp.next = null;
+            // but ab imp thing tail ko last pe leke jana hai 
+            tail = temp;
+        }
+        size++;
+    }
+
+    void addAtStart(int val){
+        Node temp = new Node(val);
+        if(head == null){
+            head = tail = temp;
+        } else {
+            temp.next = head;
+            head = temp;
+        }
+        size++;
+    }
 }
 
